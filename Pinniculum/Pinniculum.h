9edc2aac0789248b17bc260069e246f399f1b78b
kuroyam <kuroyam1109@gmail.com>
2015-02-28 23:43:03 +0900

    Initial Commit

---
+++ Pinniculum/Pinniculum.h
@@ -0,0 +1,19 @@
+//
+//  Pinniculum.h
+//  Pinniculum
+//
+//  Created by Shun Kuroda on 2015/02/28.
+//  Copyright (c) 2015年 kuroyam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pinniculum.
+FOUNDATION_EXPORT double PinniculumVersionNumber;
+
+//! Project version string for Pinniculum.
+FOUNDATION_EXPORT const unsigned char PinniculumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pinniculum/PublicHeader.h>
+
+
