a40ea2f394e800e1cd3e8fd466334f81f8960caa
Shun Kuroda <kuroyam1109@gmail.com>
2015-03-16 09:56:08 +0900

    テストをいくつか追加

--- PinniculumTests/PinniculumTests.swift
+++ PinniculumTests/PinniculumTests.swift
@@ -21,4 +21,42 @@ class PinniculumTests: XCTestCase {
         XCTAssert(attribute.pointSize == 14.0, "font size should 14pt")
     }
     
+    func testColor() {
+        let string = Pinniculum().text("color").color(UIColor(red: 0.2, green: 0.4, blue: 0.6, alpha: 1.0)).build()
+        var range : NSRange? = NSMakeRange(0, string.length)
+        let attribute : UIColor = string.attribute(NSForegroundColorAttributeName, atIndex: 0, effectiveRange: &range!) as UIColor
+        
+        var red : CGFloat = 0
+        var green : CGFloat = 0
+        var blue : CGFloat = 0
+        var alpha : CGFloat = 0
+        
+        attribute.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        XCTAssertNotNil(attribute, "attribute should not nil")
+        XCTAssert(red == 0.2, "red value should 0.2")
+        XCTAssert(green == 0.4, "green value should 0.4")
+        XCTAssert(blue == 0.6, "blue value should 0.6")
+        XCTAssert(alpha == 1.0, "alpha value should 1.0")
+    }
+    
+    func testBackgroundColor() {
+        let string = Pinniculum().text("color").backgroundColor(UIColor(red: 0.2, green: 0.4, blue: 0.6, alpha: 1.0)).build()
+        var range : NSRange? = NSMakeRange(0, string.length)
+        let attribute : UIColor = string.attribute(NSBackgroundColorAttributeName, atIndex: 0, effectiveRange: &range!) as UIColor
+        
+        var red : CGFloat = 0
+        var green : CGFloat = 0
+        var blue : CGFloat = 0
+        var alpha : CGFloat = 0
+        
+        attribute.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        XCTAssertNotNil(attribute, "attribute should not nil")
+        XCTAssert(red == 0.2, "red value should 0.2")
+        XCTAssert(green == 0.4, "green value should 0.4")
+        XCTAssert(blue == 0.6, "blue value should 0.6")
+        XCTAssert(alpha == 1.0, "alpha value should 1.0")
+    }
+    
 }
