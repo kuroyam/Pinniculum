116bb8acb92feab7c7eadcf4771e174386eac0ca
Shun Kuroda <kuroyam1109@gmail.com>
2015-03-16 13:59:36 +0900

    テストを追加

--- PinniculumTests/PinniculumTests.swift
+++ PinniculumTests/PinniculumTests.swift
@@ -12,7 +12,7 @@ import XCTest
 class PinniculumTests: XCTestCase {
     
     func testFont() {
-        let string = Pinniculum().text("font").font(UIFont(name: "Helvetica", size: 14)!).build()
+        let string = Pinniculum().text("font").font(UIFont(name: "Helvetica", size: 14.0)!).build()
         var range : NSRange? = NSMakeRange(0, string.length)
         let attribute : UIFont = string.attribute(NSFontAttributeName, atIndex: 0, effectiveRange: &range!) as UIFont
         
@@ -57,6 +57,31 @@ class PinniculumTests: XCTestCase {
         XCTAssert(green == 0.4, "green value should 0.4")
         XCTAssert(blue == 0.6, "blue value should 0.6")
         XCTAssert(alpha == 1.0, "alpha value should 1.0")
+    }
+    
+    func testKern() {
+        let string = Pinniculum().text("kern").kern(2.0).build()
+        var range : NSRange? = NSMakeRange(0, string.length);
+        let attribute : NSNumber = string.attribute(NSKernAttributeName, atIndex: 0, effectiveRange: &range!) as NSNumber
+        
+        XCTAssertNotNil(attribute, "attribute should not nil")
+        XCTAssert(attribute.floatValue == 2.0, "kern value should 2.0")
+    }
+    
+    func testAlign() {
+        let string = Pinniculum().text("align").align(NSTextAlignment.Center).build()
+        var range : NSRange? = NSMakeRange(0, string.length)
+        let style : NSParagraphStyle = string.attribute(NSParagraphStyleAttributeName, atIndex: 0, effectiveRange: &range!) as NSParagraphStyle
+        
+        XCTAssertNotNil(style, "attribute should not nil")
+        XCTAssert(style.alignment == NSTextAlignment.Center, "alignment should center");
+    }
+    
+    func testTextConnection() {
+        let string = Pinniculum().text("text1").text("text2").build();
+        var range : NSRange? = NSMakeRange(0, string.length)
+        
+        XCTAssert(string.length == "text1text2".utf16Count, "string should connected text length")
     }
     
 }
